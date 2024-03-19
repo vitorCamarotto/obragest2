40806ab9a23561039bfe63a611ee06f793ca902c
vitorCamarotto <vitorranacamarotto@gmail.com>
2024-03-19 19:41:31 -0300

    Add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Questrial: true
+    }
+  }
 })
